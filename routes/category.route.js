894f96c618f3c4818f692814dc9da020b23a763e
Mohit Sehgal <sehgal.mohit29@gmail.com>
2024-05-25 17:37:02 +0530

    Create Category

--- routes/category.route.js
+++ routes/category.route.js
@@ -18,4 +18,20 @@ router.get('/', async function (req, res, next) {
   }
 });
 
+router.post('/',async function (req, res, next) {
+  try {
+    let { title, description} = req.body;
+    let result = await db.query(`INSERT INTO categories (title,description) VALUES ($1,$2)`,[title,description])
+    res.json({
+      message: 'Record Inserted'
+    })
+
+  } catch (e) {
+    res.status(413).json({
+      message: 'Error Occurred',
+      error: e.toString()
+    })
+  }
+})
+
 module.exports = router;
