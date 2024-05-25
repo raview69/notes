df02f3859e2df9af1cb078a5d2a3140f57c4ab6d
Mohit Sehgal <sehgal.mohit29@gmail.com>
2024-05-25 17:44:04 +0530

    delete api

--- routes/category.route.js
+++ routes/category.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db')
 
-router.get('/', async function (req, res, next) {
+router.get('/', async function (req, res) {
   try {
     let result = await db.query(`SELECT * from categories`)
     let data = result.rows;
@@ -18,7 +18,7 @@ router.get('/', async function (req, res, next) {
   }
 });
 
-router.post('/',async function (req, res, next) {
+router.post('/',async function (req, res) {
   try {
     let { title, description} = req.body;
     let result = await db.query(`INSERT INTO categories (title,description) VALUES ($1,$2)`,[title,description])
@@ -34,7 +34,7 @@ router.post('/',async function (req, res, next) {
   }
 });
 
-router.get('/:id',async function (req, res, next) {
+router.get('/:id',async function (req, res) {
   try {
     let id = req.params.id;
     let result = await db.query(`SELECT * FROM categories WHERE id = $1`,[id])
@@ -55,13 +55,29 @@ router.get('/:id',async function (req, res, next) {
   }
 });
 
-router.put('/:id',async function (req, res, next) {
+router.put('/:id',async function (req, res) {
   try {
     let id = req.params.id;
     let { title, description} = req.body;
     let result = await db.query(`UPDATE categories SET title = $1, description = $2 WHERE id = $3`,[title,description,id])
     res.json({
       message: 'Record Updated'
+    })
+
+  } catch (e) {
+    res.status(413).json({
+      message: 'Error Occurred',
+      error: e.toString()
+    })
+  }
+});
+
+router.delete('/:id',async function (req, res) {
+  try {
+    let id = req.params.id;
+    let result = await db.query(`DELETE FROM categories WHERE id = $1`,[id])
+    res.json({
+      message: 'Record Deleted'
     })
 
   } catch (e) {
